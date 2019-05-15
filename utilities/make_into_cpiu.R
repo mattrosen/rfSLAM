ba4d21691e78df5686c576ade09030bf3a1c297e
Matt Rosen <mcr3@princeton.edu>
2019-05-14 19:51:06 -0500

    Some clarifying updates to make_into_cpiu.R

--- utilities/make_into_cpiu.R
+++ utilities/make_into_cpiu.R
@@ -16,18 +16,6 @@ corrected <- merge(corrected, lvef.data, all.x=TRUE)
 # compute start time of study
 corrected$start.time1 <- as.Date(as.character(corrected$dateofmri),format = "%m/%d/%Y")
 
-# add timehf1psca, etc.
-corrected$timehf1psca <- ifelse(corrected$timehf1 < corrected$timescd1, corrected$timehf1, NA)
-corrected$timehf2psca <- ifelse(corrected$timehf2 < corrected$timescd1, corrected$timehf2, NA)
-corrected$timehf3psca <- ifelse(corrected$timehf3 < corrected$timescd1, corrected$timehf3, NA)
-corrected$timehf4psca <- ifelse(corrected$timehf4 < corrected$timescd1, corrected$timehf4, NA)
-corrected$timehf5psca <- ifelse(corrected$timehf5 < corrected$timescd1, corrected$timehf5, NA)
-corrected$timehf6psca <- ifelse(corrected$timehf6 < corrected$timescd1, corrected$timehf6, NA)
-corrected$timehf7psca <- ifelse(corrected$timehf7 < corrected$timescd1, corrected$timehf7, NA)
-corrected$timehf8psca <- ifelse(corrected$timehf8 < corrected$timescd1, corrected$timehf8, NA)
-corrected$timehf9psca <- ifelse(corrected$timehf9 < corrected$timescd1, corrected$timehf9, NA)
-corrected$timehf10psca <- ifelse(corrected$timehf10 < corrected$timescd1, corrected$timehf10, NA)
-
 # add for lvef
 corrected$nuc1date  <- corrected$nucdate
 corrected$nuc1ef    <- corrected$nucef
@@ -47,6 +35,7 @@ corrected$min.death.hf <- pmin(corrected$deathtime,
 lvef.list <- list(times=c(sprintf("echo%ddate", 0:11), sprintf("nuc%ddate", 1:3)), 
 				  values=c(sprintf("echo%def", 0:11),  sprintf("nuc%def", 1:3)))
 
+# list of variables to create for CPIU'd data ending at sca1
 to.create.msc <- list(t.hf=sprintf("timehf",seq(1:10)),
 					  t.sca="timescd1",
 					  c.lvef=lvef.list,
@@ -81,6 +70,7 @@ to.create.mdeath <- list(t.hf=sprintf("timehf%dpsca",seq(1:10)),
 						 ni.hf=sprintf("timehf%dpsca",seq(1:10)),
 						 ni.sca="timescd1")
 
+# make the CPIUs
 for (k in c(0, 2)) {
 
 	# new files
